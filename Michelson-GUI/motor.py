04d9088ba517893c25c14f6c4fed07c969959ede
Simon Lizotte <simon.lizotte.1@ulaval.ca>
2022-04-18 17:46:10 -0400

    improve documentation

--- Michelson-GUI/motor.py
+++ Michelson-GUI/motor.py
@@ -13,33 +13,46 @@ jog_settings = {
             "stop mode"   : 0x0002   # smooth stop
         }
 
-unit_conversion = {
-            # "ZST225B": { "MU/mm": 2008645.63, "MU/(mm/s)": 107824097.5, "MU/(mm/s^2)": 22097.3} Incorrect factor?
-            "ZST225B": { "MU/mm": 2008645.63/0.26886, "MU/(mm/s)": 107824097.5/0.26886, "MU/(mm/s^2)": 22097.3/0.26886}
-        }
-
-
 
 class Motor:
     """
-    Object that sends to and receive data from a ThorLabs' motor. ThorLabs uses it's own messaging protocol
-    called APT/Kinesis. This protocal is described here https://www.thorlabs.com/Software/Motion%20Control/APT_Communications_Protocol.pdf
+    Object that sends to and receive data from a ThorLabs' motor.
 
-    The main idea is that messages are sent within a sequence of bits of varying length. Data is sent via
-    the "write" method of a serial object assigned to the specific motor. The first argument of this "write"
-    method contains the type of data sent in the message. "H" stands for a unsigned 16 bits integer, "B" for
-    8 bits (1 byte) and "I" for a long signed 32 bits integer. Types are described at page 38 (as of Issue 27).
-    The order of the bytes are in little-endian format so care must be taken when assigning values.
+    ThorLabs uses it's own messaging protocol called APT. This protocol is described here https://www.thorlabs.com/Software/Motion%20Control/APT_Communications_Protocol.pdf
+    This manual also present the available instructions for each type of motor and describes how they behave.
+    Here, we explain in summary how it works.
 
-    The other arguments of the "write" method correspond to the data sent for each type given in the same order.
-    Every message starts with a header of 16 bits ("H"). The other arguments are optionnal and depend on the
-    task.
+    Messages are a sequence of bits of varying length. They are sent via the "write" method of the "serial" object
+    connected to the given motor USB port.
+
+    To create a message, we use Python's "struct.pack" function. The first argument contains a format string in
+    which each character represent the type of parameter. "B" stands for 8 bits, "H" for 16-bit unsigned integer
+    and "I" for a 32-bit signed integer. Complete list can be found at https://docs.python.org/3/library/struct.html
+    Because motors read little-endian values, the character '<' must be added in the format string. If we want
+    to send a message containing a 16-bit unsigned integer followed by a byte (8 bits), we use "<HB" as the format
+    string:
+
+        struct.pack("<HB", ...)
+
+    Types used in messages are described at page 39 of ThorLabs' documentation (as of Issue 30). The second argument
+    of "struck.pack" is the list of values to store in the sequence (using the types of the format string).
+
+    In the APT protocol, every message must start the ID of the instruction, a 16-bit unsigned integer ("H"). The
+    other bits are optional: they depend on the instruction asked (read the instruction's description in the manual).
+
+    The motors represents positions in their intrinsic integer unit which we call MU. The motor class provides the
+    conversion factors for each unit used.
     """
+
+    unit_conversion = {
+                # "ZST225B": { "MU/mm": 2008645.63, "MU/(mm/s)": 107824097.5, "MU/(mm/s^2)": 22097.3} Incorrect factor?
+                "ZST225B": { "MU/mm": 2008645.63/0.26886, "MU/(mm/s)": 107824097.5/0.26886, "MU/(mm/s^2)": 22097.3/0.26886}
+            }
 
     def __init__(self, usb_port_location, motor_name):
         # Command Protol PDF can be found https://www.thorlabs.com/Software/Motion%20Control/APT_Communications_Protocol.pdf
 
-        self.unit_conversions = unit_conversion[motor_name]
+        self.unit_conversions = self.unit_conversion[motor_name]
 
         # Port Settings
         baud_rate = 115200
@@ -59,21 +72,27 @@ class Motor:
 
         self.enable_stage()
 
+
     def flush(self):
         self.communicator.flushInput()
         self.communicator.flushOutput()
 
 
+    def _ensure_motor_received_instruction(self):
+        time.sleep(0.1)
+
+
     def enable_stage(self):
         # Enable Stage; MGMSG_MOD_SET_CHANENABLESTATE
         self.communicator.write(pack('<HBBBB', 0x0210, self.channel, 0x01, self.destination, self.source))
-        time.sleep(0.1)
+        self._ensure_motor_received_instruction()
 
 
     def disable_stage(self):
         # Disable Stage; MGMSG_MOD_SET_CHANENABLESTATE
         self.communicator.write(pack('<HBBBB', 0x0210, self.channel, 0x02, self.destination, self.source))
-        time.sleep(0.1)
+        self._ensure_motor_received_instruction()
+
         self.flush()
 
 
@@ -95,15 +114,15 @@ class Motor:
                         self.convert_acceleration_to_MU(jog_settings["acceleration"]),
                         self.convert_velocity_to_MU(jog_settings["max velocity"]),
                         jog_settings["stop mode"]))
-        time.sleep(0.1)
+        self._ensure_motor_received_instruction()
 
 
     def jog(self):
         # Do a jog; MGMSG_MOT_MOVE_JOG
         self.communicator.write(pack('<HBBBB', 0x046A, self.channel, 0x01, self.destination, self.source))
-        time.sleep(0.1)
-
+        self._ensure_motor_received_instruction()
         self._wait_until_move_completed()
+
 
     def move_to(self, position, relative=True):
         if relative:
@@ -114,9 +133,10 @@ class Motor:
             # Move to absolute position; MGMSG_MOT_MOVE_ABSOLUTE (long version)
             self.communicator.write(pack('<HBBBBHI', 0x0453, 0x06, 0x00, self.destination|0x80, self.source, self.channel,
                               self.convert_position_to_MU(position)))
-        time.sleep(0.1)
 
+        self._ensure_motor_received_instruction()
         self._wait_until_move_completed()
+
 
     def _wait_until_move_completed(self):
         Rx = ''
@@ -126,21 +146,26 @@ class Motor:
             Rx = self.communicator.read(2)
             time.sleep(0.1)
 
+
     def stop(self):
         # Stop any movement; MGMSG_MOT_MOVE_STOP
         self.communicator.write(pack('<HBBBB',0x0465, 0x02, self.channel, self.destination, self.source))
+
 
     def convert_position_to_MU(self, position):
         """ position in mm """
         return int(round(self.unit_conversions["MU/mm"]*position))
 
+
     def convert_MU_to_position(self, position):
         """ position in mm """
         return position/self.unit_conversions["MU/mm"]
 
+
     def convert_velocity_to_MU(self, velocity):
         """ velocity in mm/s^2 """
         return int(round(self.unit_conversions["MU/(mm/s)"]*velocity))
+
 
     def convert_acceleration_to_MU(self, acceleration):
         """ acceleration in mm/s^2 """
